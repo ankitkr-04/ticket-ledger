90f8011536d89cf2acb272585f41a50d1acc2868
ankit <ak0182274@gmail.com>
2026-02-11 08:08:51 +0530

    feat: implement async configuration with custom exception handler and metrics tracking

--- src/main/java/com/ticketledger/config/AsyncConfig.java
+++ src/main/java/com/ticketledger/config/AsyncConfig.java
@@ -1,10 +1,38 @@
 package com.ticketledger.config;
 
+import java.util.concurrent.Executor;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.ticketledger.exception.AsyncExceptionHandler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @EnableAsync
 @Configuration
-public class AsyncConfig {
+@Slf4j
+@RequiredArgsConstructor
+public class AsyncConfig implements AsyncConfigurer {
+    private static final String THREAD_NAME_PREFIX = "async-vt-";
+    private final AsyncExceptionHandler asyncExceptionHandler;
+
+    @Override
+    public @Nullable Executor getAsyncExecutor() {
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor(THREAD_NAME_PREFIX);
+        executor.setVirtualThreads(true);
+
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return asyncExceptionHandler;
+    }
 
 }
---
+++ src/main/java/com/ticketledger/exception/AsyncExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.ticketledger.exception;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.stereotype.Component;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AsyncExceptionHandler implements AsyncUncaughtExceptionHandler {
+    private final MeterRegistry meterRegistry;
+
+    @Override
+    public void handleUncaughtException(Throwable ex, java.lang.reflect.Method method, Object... params) {
+        log.error("Async error in method: {}", method.getName(), ex.getMessage(),
+                Arrays.toString(params), ex);
+        meterRegistry.counter("async.execution.failure",
+
+                List.of(
+                        Tag.of("method", method.getName()),
+                        Tag.of("exception", ex.getClass().getSimpleName())))
+                .increment();
+    }
+}
